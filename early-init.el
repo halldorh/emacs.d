1ceb9ccba1f0b820642f4e7de82beaf86b37e332
Halldor Runar Haflidason <hrh@aix.is>
2023-11-07 09:10:50 +0000

    Add nongnu elpa package.

--- early-init.el
+++ early-init.el
@@ -9,6 +9,8 @@
 ;;; Code:
 
 (setq package-enable-at-startup nil)
+(with-eval-after-load 'package
+  (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 
 ;; So we can detect this having been loaded
 (provide 'early-init)
